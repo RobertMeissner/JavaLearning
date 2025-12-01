891c101bbf9c9c3b25925c96cbd2bfe7b85c09dc
Robert Meissner <50482279+RobertMeissner@users.noreply.github.com>
2025-12-01 13:40:59 +0100

    fix: Fix p088 test and pom

--- src/test/java/org/leetcode/p088Test.java
+++ src/test/java/org/leetcode/p088Test.java
@@ -2,8 +2,6 @@ package org.leetcode;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class p088Test {
